2c50ca39cf4fcce152bb597daf9dbd84cf5c9029
Elisa <codersquirrelbln@gmail.com>
2019-11-06 13:45:35 +0100

    add modal for contact change styling add responsiveness

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,24 +1,26 @@
-let coll = document.getElementsByClassName("collapsible");
-// console.log(coll);
-// let minimize = document.querySelector('#minimze');
-let i;
+// let coll = document.getElementsByClassName("collapsible");
+// // console.log(coll);
+// // let minimize = document.querySelector('#minimze');
+// let i;
 
-console.log(coll);
-for (i = 0; i < coll.length; i++) {
-  console.log('hello');
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    console.log(content);
-    if (content.style.display === "block") {
-      console.log('if statement');
-      content.style.display = "none";
-    } else {
-      console.log('else statement');
-      content.style.display = "block";
-    }
-  });
-}
+// console.log(coll);
+// for (i = 0; i < coll.length; i++) {
+//   console.log('hello');
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     let content = this.nextElementSibling;
+//     console.log(content);
+//     if (content.style.display === "block") {
+//       console.log('if statement');
+//       content.style.display = "none";
+//     } else {
+//       console.log('else statement');
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+
 
 // for (j = 0; j < collVisa.length; i++) {
 //   collVisa[i].addEventListener("click", function() {
